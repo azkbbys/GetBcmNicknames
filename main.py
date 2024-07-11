432ac17954701884261ec36c84cee11e86bc1992
阿兹卡班毕业生 <oyroyroyr@163.com>
2024-07-11 18:50:45 +0800

    最小id调至1w

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ def get_nickname(user_id):
     try:
         nickname = loads(get(f'https://api.codemao.cn/creation-tools/v1/user/center/honor?user_id={user_id}').text)['nickname']
         namelist.append(nickname)
-        print(nickname+str(user_id))
+        print(nickname+' '+str(user_id))
     except:
         pass
 
-i = 1000
-while True:
+i = 10000
+while i <= 19135000:
     threads = []
     for _ in range(100):
         thread = threading.Thread(target=get_nickname, args=(i,))
@@ -22,8 +22,6 @@ while True:
         i += 1
     for thread in threads:
         thread.join()
-    if i > 19135000:
-        break
 
 # 保存到文件
 with open('nickname.txt', 'w', encoding='utf-8') as f:
